89dc45a5473c8a499a74e1f1dd1bbe45f20180d2
ivan ortega <ivan.ortega.asensio@gmail.com>
2023-09-20 23:41:34 +0200

    modify api

--- wishlistraulApi/index.js
+++ wishlistraulApi/index.js
@@ -125,7 +125,7 @@ app.post('/wishlists', verifyToken, (req, res) => {
       }
     });
   });
-    // Ruta para marcar un producto como comprado
+    // Ruta para marcar un producto como no comprado
     app.put('/wishlists/:wishlistId/products/:productId/unbuy', verifyToken, (req, res) => {
       const wishlistId = req.params.wishlistId;
       const productId = req.params.productId;
@@ -142,47 +142,107 @@ app.post('/wishlists', verifyToken, (req, res) => {
       });
     });
   
+
   // Ruta para eliminar una lista de deseos
-  app.delete('/wishlists/:wishlistId', verifyToken, (req, res) => {
-    const wishlistId = req.params.wishlistId;
-  
-    const sql = 'DELETE FROM wishlists WHERE id = ?';
-    db.query(sql, [wishlistId], (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Error al eliminar la lista de deseos' });
-      } else if (result.affectedRows === 0) {
-        res.status(404).json({ error: 'Lista de deseos no encontrada' });
+app.delete('/wishlists/:wishlistId', verifyToken, (req, res) => {
+  const wishlistId = req.params.wishlistId;
+  const userId = req.user.id;
+
+  // Check if the wishlist is shared
+  const checkSharedQuery = 'SELECT * FROM wishlist_members WHERE wishlist_id = ? AND user_id = ?';
+
+  db.query(checkSharedQuery, [wishlistId, userId], (err, sharedResults) => {
+    if (err) {
+      res.status(500).json({ error: 'Error al verificar si la lista de deseos está compartida' });
+    } else {
+      // If the wishlist is shared, remove the link (shared member)
+      if (sharedResults.length > 0) {
+        const removeLinkQuery = 'DELETE FROM wishlist_members WHERE wishlist_id = ? AND user_id = ?';
+
+        db.query(removeLinkQuery, [wishlistId, userId], (err, result) => {
+          if (err) {
+            res.status(500).json({ error: 'Error al eliminar el enlace compartido' });
+          } else if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'No tienes permiso para eliminar el enlace compartido' });
+          } else {
+            res.status(200).json({ message: 'Enlace compartido eliminado' });
+          }
+        });
       } else {
-        res.status(200).json({ message: 'Lista de deseos eliminada' });
+        // If the wishlist is not shared, directly delete it
+        const deleteWishlistQuery = 'DELETE FROM wishlists WHERE id = ? AND moderator_id = ?';
+
+        db.query(deleteWishlistQuery, [wishlistId, userId], (err, result) => {
+          if (err) {
+            res.status(500).json({ error: 'Error al eliminar la lista de deseos' });
+          } else if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Lista de deseos no encontrada o no tienes permiso para eliminarla' });
+          } else {
+            res.status(200).json({ message: 'Lista de deseos eliminada' });
+          }
+        });
       }
-    });
+    }
   });
+});
+
 
   // Ruta para eliminar un producto de una lista
-  app.delete('/wishlists/:wishlistId/products/:productId', verifyToken, (req, res) => {
-    const wishlistId = req.params.wishlistId;
-    const productId = req.params.productId;
+app.delete('/wishlists/:wishlistId/products/:productId', verifyToken, (req, res) => {
+  const wishlistId = req.params.wishlistId;
+  const productId = req.params.productId;
+  const userId = req.user.id;
 
-    const sql = 'DELETE FROM products WHERE id = ? AND wishlist_id = ?';
-    db.query(sql, [productId, wishlistId], (err, result) => {
+  // Check if the user making the request is the moderator of the wishlist
+  const checkModeratorQuery = 'SELECT moderator_id FROM wishlists WHERE id = ?';
+
+  db.query(checkModeratorQuery, [wishlistId], (err, result) => {
       if (err) {
-        res.status(500).json({ error: 'Error al eliminar el producto' });
-      } else if (result.affectedRows === 0) {
-        res.status(404).json({ error: 'Producto o lista no encontrados' });
-      } else {
-        res.status(200).json({ message: 'Producto eliminado' });
+          return res.status(500).json({ error: 'Error al verificar el moderador de la lista de deseos' });
       }
-    });
+
+      if (result.length === 0) {
+          return res.status(404).json({ error: 'Lista de deseos no encontrada' });
+      }
+
+      const moderatorId = result[0].moderator_id;
+
+      if (userId !== moderatorId) {
+          return res.status(403).json({ error: 'No tienes permiso para eliminar este producto' });
+      }
+
+      // If the user is the moderator, proceed to delete the product
+      const deleteProductQuery = 'DELETE FROM products WHERE id = ? AND wishlist_id = ?';
+
+      db.query(deleteProductQuery, [productId, wishlistId], (err, result) => {
+          if (err) {
+              res.status(500).json({ error: 'Error al eliminar el producto' });
+          } else if (result.affectedRows === 0) {
+              res.status(404).json({ error: 'Producto o lista no encontrados' });
+          } else {
+              res.status(200).json({ message: 'Producto eliminado' });
+          }
+      });
   });
+});
+
 
   // Ruta para obtener todas las listas de deseos de un usuario
 app.get('/wishlists', verifyToken, (req, res) => {
     const userId = req.user.id;
   
     const sql = `
-      SELECT w.id, w.name, w.moderator_id, w.created_at
-      FROM wishlists w
-      WHERE w.moderator_id = ?
+        SELECT w.id, w.name, w.moderator_id, w.created_at, 
+        CASE
+          WHEN EXISTS (SELECT 1 FROM products p WHERE p.wishlist_id = w.id AND p.purchased = 0) THEN true
+          ELSE false
+        END AS hasProducts,
+        CASE
+          WHEN EXISTS (SELECT 1 FROM wishlist_members wm WHERE wm.wishlist_id = w.id) THEN true
+          ELSE false
+        END AS shared
+        FROM wishlists w
+        WHERE w.moderator_id = ?
     `;
     db.query(sql, [userId], (err, results) => {
       if (err) {
@@ -198,9 +258,10 @@ app.get('/wishlists', verifyToken, (req, res) => {
     const userId = req.user.id;
   
     const sql = `
-      SELECT w.id, w.name, w.moderator_id, w.created_at
+      SELECT w.id, w.name, u.username as moderator_id, w.created_at, wm.attend
       FROM wishlists w
       INNER JOIN wishlist_members wm ON wm.wishlist_id = w.id
+      INNER JOIN users u ON u.id = w.moderator_id
       WHERE wm.user_id = ?
     `;
     db.query(sql, [userId], (err, results) => {
@@ -211,6 +272,41 @@ app.get('/wishlists', verifyToken, (req, res) => {
       }
     });
   });
+
+
+  // Ruta para obtener la lista de usuarios compartidos en una lista de deseos
+  app.get('/wishlists/:wishlistId/shared-users', verifyToken, (req, res) => {
+    const wishlistId = req.params.wishlistId;
+
+    // Verifica si el usuario autenticado es el moderador de la lista de deseos
+    const moderatorId = req.user.id;
+    const checkModerator = 'SELECT id FROM wishlists WHERE id = ? AND moderator_id = ?';
+
+    db.query(checkModerator, [wishlistId, moderatorId], (err, result) => {
+      if (err) {
+        res.status(500).json({ error: 'Error al verificar el moderador de la lista de deseos' });
+      } else if (result.length === 0) {
+        res.status(403).json({ error: 'No tienes permiso para acceder a esta lista de deseos' });
+      } else {
+        // Si el usuario es el moderador, busca los usuarios compartidos en la lista de deseos
+        const sql = `
+          SELECT u.id, u.username, u.email, wm.attend
+          FROM users u
+          INNER JOIN wishlist_members wm ON u.id = wm.user_id
+          WHERE wm.wishlist_id = ?
+        `;
+
+        db.query(sql, [wishlistId], (err, results) => {
+          if (err) {
+            res.status(500).json({ error: 'Error al obtener la lista de usuarios compartidos' });
+          } else {
+            res.status(200).json({ sharedUsers: results });
+          }
+        });
+      }
+    });
+  });
+
 
   // Ruta para compartir una lista de deseos con un usuario
 app.post('/wishlists/:wishlistId/share', verifyToken, (req, res) => {
@@ -284,6 +380,70 @@ function verifyToken(req, res, next) {
     }
   }
 
+  // Ruta para editar la información del usuario
+  app.put('/users/:id', verifyToken, (req, res) => {
+    const userId = req.params.id;
+    const { username, email, theme } = req.body;
+
+    // Check if the authenticated user is the same as the user being edited
+    if (req.user.id !== parseInt(userId)) {
+        return res.status(403).json({ error: 'No tienes permiso para editar este usuario' });
+    }
+
+    const updateUserQuery = 'UPDATE users SET username = ?, email = ?, theme = ? WHERE id = ?';
+    
+    db.query(updateUserQuery, [username, email, theme, userId], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Error al editar la información del usuario' });
+        } else if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Usuario no encontrado' });
+        } else {
+            res.status(200).json({ message: 'Información de usuario actualizada' });
+        }
+    });
+  });
+
+  // Ruta para actualizar el estado de asistencia a una lista de deseos
+app.put('/wishlists/:wishlistId/attend', verifyToken, (req, res) => {
+  const userId = req.user.id;
+  const wishlistId = req.params.wishlistId;
+  const { assist } = req.body;
+
+  // Update the 'assist' status for the user and wishlist
+  const updateAttendStatusQuery = 'UPDATE wishlist_members SET attend = ? WHERE wishlist_id = ? AND user_id = ?';
+
+  db.query(updateAttendStatusQuery, [assist, wishlistId, userId], (err, result) => {
+      if (err) {
+          res.status(500).json({ error: 'Error al actualizar el estado de asistencia' });
+      } else if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'No tienes permiso para actualizar la asistencia o la lista de deseos no se encuentra' });
+      } else {
+          res.status(200).json({ message: 'Estado de asistencia actualizado' });
+      }
+  });
+});
+
+// Ruta para eliminar la cuenta de usuario
+app.delete('/users/:id', verifyToken, (req, res) => {
+  const userId = req.params.id;
+
+  // Check if the authenticated user is the same as the user being deleted
+  if (req.user.id !== parseInt(userId)) {
+      return res.status(403).json({ error: 'No tienes permiso para eliminar esta cuenta de usuario' });
+  }
+
+  const deleteUserQuery = 'DELETE FROM users WHERE id = ?';
+
+  db.query(deleteUserQuery, [userId], (err, result) => {
+      if (err) {
+          res.status(500).json({ error: 'Error al eliminar la cuenta de usuario' });
+      } else if (result.affectedRows === 0) {
+          res.status(404).json({ error: 'Usuario no encontrado' });
+      } else {
+          res.status(200).json({ message: 'Cuenta de usuario eliminada' });
+      }
+  });
+});
 
 
 // Iniciar el servidor
